c10843913dc8a9a7ab0b767af55ed3cc97f4b7e8
Mvlimaa <marcelovynicius@gmail.com>
2025-05-07 16:46:50 -0300

    READY

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
-import { Text } from "react-native";
+import { Button, Text, View } from "react-native";
 
 export default function Login() {
     return (
+        <View>
 
-        <Text>Olá</Text>
 
+        <Text>hello word!! 2</Text>
+
+        <Text></Text>
+
+        </View>
     )
 };
